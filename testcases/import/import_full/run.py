f4b0a349a84c1a4132f6ce0403f96d0527832821
JamesOsgood <james.osgood@gmail.com>
2020-06-01 15:16:06 +0100

    Choose files

--- testcases/import/import_full/run.py
+++ testcases/import/import_full/run.py
@@ -9,12 +9,15 @@ class PySysTest(EYPIBaseTest):
 
 	def execute(self):
 		
+		# instances = [1,2,5,10]
+		instances = [1,2,5,10]
 		dir = os.path.expanduser(os.path.join(self.project.DATA_DIR, 'processed'))
 		for instance_id, file in self.getFilesToProcess(dir):
 			start = datetime.now()
-			self.importFileMongoImport(file, f"records_{instance_id}")
-			time_taken = datetime.now() - start
-			self.write_test_result(instance_id, time_taken.total_seconds())
+			if instance_id in instances:
+				self.importFileMongoImport(file, f"records_{instance_id}")
+				time_taken = datetime.now() - start
+				self.write_test_result(instance_id, time_taken.total_seconds())
 
 	def validate(self):
 		pass
